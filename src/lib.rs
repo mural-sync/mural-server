5a71d2aba133c18461e54ca132e93768b952cb87
Silvan Schmidt <mail@silvanschmidt.ch>
2025-03-22 10:41:35 +0100

    feat: not found

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod config;
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, http::StatusCode, web};
 pub(crate) use config::Config;
 
 mod env;
@@ -19,17 +19,25 @@ pub(crate) use state::State;
 mod wallpaper;
 pub(crate) use wallpaper::Wallpaper;
 
+async fn not_found() -> impl Responder {
+    HttpResponse::build(StatusCode::NOT_FOUND).body("Not Found")
+}
+
 pub async fn run() -> Result<()> {
     env::load_dotenv()?;
     let config = Config::load()?;
     let state = State::new(&config)?;
 
-    HttpServer::new(move || App::new().app_data(web::Data::new(state.clone())))
-        .bind(("0.0.0.0", config.port()))
-        .unwrap()
-        .run()
-        .await
-        .unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(state.clone()))
+            .default_service(web::route().to(not_found))
+    })
+    .bind(("0.0.0.0", config.port()))
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
 
     Ok(())
 }
